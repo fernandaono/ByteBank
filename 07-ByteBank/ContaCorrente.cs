66cb7bae82fd18bfa45f4792cdeb7242c7ea99a9
Fernanda Ono <fernanda.ono@avanade.com>
2020-09-10 12:12:27 -0300

    construtores

---
+++ 07-ByteBank/ContaCorrente.cs
@@ -0,0 +1,91 @@
+﻿
+namespace _07_ByteBank
+{
+	public class ContaCorrente
+	{
+		private int _Agencia;
+		public Cliente Titular { get; set; }
+		public static int TotalDeContasCriadas { get; private set; }
+
+		public int Agencia
+		{
+			get
+			{
+				return _Agencia;
+
+			}
+			set
+			{
+				if (value <= 0)
+				{
+					return;
+				}
+
+				_Agencia = value;
+			}
+		}
+		public int Numero { get; set; }
+		public double _Saldo = 100;
+
+		public double Saldo
+		{
+			get
+			{
+				return _Saldo;
+			}
+			set
+			{
+				if (value < 0)
+				{
+					return;
+				}
+
+				_Saldo = value;
+			}
+		}
+
+		public ContaCorrente(int agencia, int numero)
+		{
+			Agencia = agencia;
+			Numero = numero;
+
+			TotalDeContasCriadas++;
+		}
+
+
+		public bool Sacar(double valor)
+		{
+			if (_Saldo < valor)
+			{
+				return false;
+			}
+			else
+			{
+				_Saldo -= valor;
+				return true;
+			}
+
+		}
+
+		public void Depositar(double valor)
+		{
+			_Saldo += valor;
+		}
+
+		public bool Transferir(double valor, ContaCorrente contaDestino)
+		{
+			if (_Saldo < valor)
+			{
+				return false;
+			}
+			else
+			{
+				_Saldo -= valor;
+				contaDestino.Depositar(valor);
+				return true;
+			}
+		}
+
+	}
+}
+
---
+++ 07-ByteBank/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ByteBank
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			ContaCorrente conta = new ContaCorrente(1256, 48846);
+			Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+			Console.WriteLine(conta.Agencia);
+			Console.WriteLine(conta.Numero);
+
+			ContaCorrente contaDaGabriela = new ContaCorrente(1256, 144789);
+
+			Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+			Console.ReadLine();
+		}
+	}
+}
